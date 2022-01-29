51f89958efa244227fee97afbfaf7570f11eaded
ahmedezzaouia <35857662+ahmedezzaouia@users.noreply.github.com>
2022-01-29 13:10:37 +0100

    init haydrogen

---
+++ src/components/Footer.server.jsx
@@ -0,0 +1,12 @@
+import FooterElm from './footer/footer';
+
+/**
+ * A server component that specifies the content of the footer on the website
+ */
+export default function Footer() {
+  return (
+    <footer role="contentinfo">
+      <FooterElm></FooterElm>
+    </footer>
+  );
+}
